e0ef5a360ba4ca3e8608c376bb5d83ca0ae679cd
raihan <raihan.yusri@gmail.com>
2023-01-06 16:31:49 +0800

    vercel fix 3

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.use("/static", express.static(path.resolve(__dirname, "frontend", "static"))
 app.use(express.static('frontend'));
 
 app.get("/*", (req, resp) => {
-    resp.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    // resp.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    resp.sendFile('index.html', {root: path.join(__dirname, 'frontend')});
 });
 
 app.listen(process.env.PORT || 9999, () => console.log("Server running!"));
