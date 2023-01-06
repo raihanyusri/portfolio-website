449cc3e638e8fbf6a56d64595fc1b5e2fa981b44
raihan <raihan.yusri@gmail.com>
2023-01-06 22:38:25 +0800

    added project links

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -170,7 +170,9 @@ class Projects extends AbstractView {
 
             <div class="all_proj">
             <div class="box">
-                <img class="proj_img" src="/static/img/portfolio.png" /></br>
+                <a href="https://github.com/raihanyusri/portfolio-website" target="_blank"> 
+                    <img class="proj_img" src="/static/img/portfolio.png" /></br>
+                </a></br>
                 <span class="experience_company">Personal Portfolio Website</span></br>
                 <span class="proj_desc">The website that you are on right now! Instead of using state of the art technologies like React, I wanted to 
                 greater appreciate the language of the web - and built this website with vanilla JavaScript, HTML and CSS.</span></br></br>
@@ -180,7 +182,9 @@ class Projects extends AbstractView {
                 <img class="tech_stack" src= "/static/img/css.png" title="CSS"/>
             </div>
             <div class="box">
-                <img class="proj_img" src="/static/img/nusociety1.png" /></br>
+                <a href="https://github.com/lyrador/nusociety" target="_blank"> 
+                    <img class="proj_img" src="/static/img/nusociety1.png" /></br>
+                </a></br>
                 <span class="experience_company">NUSociety</span></br>
                 <span class="proj_desc">A co-curricular activities portal for university students to use - with features to manage societies, events, posts, comments and more! 
                 Built for IS3106: Enterprise Systems Interface Design and Development. (Grade attained: A) </span></br></br>
@@ -196,7 +200,9 @@ class Projects extends AbstractView {
 
             <div class="all_proj">
             <div class="box">
-                <img class="proj_img" src="/static/img/cookwhat.png" /></br>
+                <a href="https://github.com/raihanyusri/recipe-searcher" target="_blank"> 
+                    <img class="proj_img" src="/static/img/cookwhat.png" />
+                </a></br>
                 <span class="experience_company">CookWhat?</span></br>
                 <span class="proj_desc">A web application for inspiring cooks to search for recipes and add them to favourites for future reference, 
                 if they are logged in.</span></br></br>
@@ -207,7 +213,9 @@ class Projects extends AbstractView {
             </div>
 
             <div class="box">
-                <img class="proj_img" src="/static/img/bt2102.png" /></br>
+                <a href="https://github.com/raihanyusri/als" target="_blank"> 
+                    <img class="proj_img" src="/static/img/bt2102.png" /></br>
+                </a></br>
                 <span class="experience_company">A Library System (ALS)</span></br>
                 <span class="proj_desc">A desktop-based GUI application to keep records of books and and provide services for library members to borrow, return, and reserve books. 
                 Built for BT2102: Data Management and Visualization. (Attained maximum score: 30/30)</span></br></br>
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-// app.use("/static", express.static(path.resolve(__dirname, "public", "static")));
+app.use("/static", express.static(path.resolve(__dirname, "public", "static")));
 app.use(express.static('public'));
 
 app.get("/*", (req, resp) => {
