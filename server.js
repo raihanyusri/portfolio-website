0e4cea4a68f375f077398169e5ea39bc0e52e37d
raihan <raihan.yusri@gmail.com>
2023-01-06 21:20:11 +0800

    vercel test

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
-app.use(express.static('frontend'));
+// app.use("/static", express.static(path.resolve(__dirname, "public", "static")));
+app.use(express.static('public'));
 
 app.get("/*", (req, resp) => {
     // resp.sendFile(path.resolve(__dirname, "frontend", "index.html"));
-    resp.sendFile('index.html', {root: path.join(__dirname, 'frontend')});
+    resp.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
 
 app.listen(process.env.PORT || 9999, () => console.log("Server running!"));
